07c9d93610df0595d70059a8c024291c1712928d
juank27 <juankruiz27@gmail.com>
2021-11-02 21:44:27 -0500

    syntaxis basica de python

--- hola.py
+++ hola.py
@@ -1,4 +1,20 @@
+"""
 print("Hola desde python")
 print("tiros")
 print("El propio")
 print("guayabo eterno")
+"""
+
+numero_uno = 6
+numero_dos = 6
+suma = numero_uno % numero_dos
+#print(suma)
+x = 5
+#print(x)
+
+#Condicionales
+if numero_uno != numero_dos :
+   print("son distintos")
+else:
+   print("iguales")
+
