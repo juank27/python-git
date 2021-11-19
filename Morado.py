b57d808f07d6636c747110793efbf678f66564a7
nataga-10 <natuchis.gutierrez@gmail.com>
2021-11-18 21:06:33 -0500

    Comente las partes importantes, para realizar un while

--- Morado.py
+++ Morado.py
@@ -1,8 +1,9 @@
-print("El morado hace parte tambien de mi")
+print("El morado hace parte tambien de mi") 
 print("Mi primer ciclo en python")
-i = 0
-while (i<= 6):
+i = 0 #Inicializacion de variable 
+
+while (i<= 6): #Condicion
     print(i)
-    i = i + 1
+    i = i + 1 #Acumulador 
 
 print ("Termine mi primer ciclo")
