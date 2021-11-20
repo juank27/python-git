6936ebf82aad1775bfa94d87f22207a60eba19db
juank27 <juankruiz27@gmail.com>
2021-11-19 21:15:01 -0500

    while modificado

--- ciclo-while.py
+++ ciclo-while.py
@@ -1,6 +1,18 @@
-
+#ciclo while
 print("Iniciando")
 i = 0
 while (i <= 6):
     print(i)
     i = i + 1
+
+# modificando wl while
+print("while modificado")
+j = 0
+bandera = True
+while (bandera):
+    #print("Entre una vez al cilco modificado")
+    j = int(input("Dijite un numero: "))
+    print(j)
+    #j = j + 1
+    if(j == 3):
+        bandera = False
