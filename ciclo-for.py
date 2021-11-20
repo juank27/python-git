4d76b58c8c86b86cd20b61c4f20b3eecf5c87d9d
juank27 <juankruiz27@gmail.com>
2021-11-19 21:58:48 -0500

    bucle for

---
+++ ciclo-for.py
@@ -0,0 +1,8 @@
+#for y listas
+#listas de frutas
+import math
+test = math.inf
+frutas = ['naranjas', "manzana", "guayaba", "pera"]
+#range(inicio,final,pasos)
+for f in range(10, 0, -1):
+    print(f)
